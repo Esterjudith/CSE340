22a29ab2d570bafb50626b6db4666f895803f462
Judy Hanson <judy.l.hanson@gmail.com>
2025-09-21 16:56:46 -0700

    Wk 3 assignment

---
+++ routes/inventoryRoute.js
@@ -0,0 +1,14 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const invController = require("../controllers/invController")
+const utilities = require("../utilities/")
+
+// Route to build inventory by classification view\
+router.get("/detail/:inv_id", utilities.handleErrors(invController.buildDetailView));
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+
+//Error router
+router.get("/causeError", utilities.handleErrors(invController.throwError))
+
+module.exports = router;
